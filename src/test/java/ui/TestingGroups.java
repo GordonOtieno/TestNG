e0d730059f19f4fb6a964e6164b45d75488d0faa
gordonotieno <otienogordon95@gmail.com>
2024-02-16 23:50:22 +0300

    update the project

--- src/test/java/ui/TestingGroups.java
+++ src/test/java/ui/TestingGroups.java
@@ -4,6 +4,7 @@ import static org.testng.Assert.assertEquals;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
 import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.Test;
 import org.testng.asserts.Assertion;
@@ -25,8 +26,10 @@ public class TestingGroups {
 	
 	@Test(priority = 2, groups = "abc")
 	public void testTwo() {
+		Reporter.log("This is  a test 2 log");
 	 System.out.println("Testing Two");
 	}
+	
 	@Test(priority = 3, groups = {"abc","cde"})
 	public void testThree() {
 	 System.out.println("Testing Three");
